d05b9b123fa55ff86ce2e51c5fa2058ba48fd720
Karen Baque <k.baque@aol.com>
2017-08-03 09:57:35 -0400

    music.sql

---
+++ music-app/db/seeds/music.sql
@@ -0,0 +1,82 @@
+\connect music_dev
+
+INSERT INTO music (mood, title, artist, genre) VALUES
+(
+  'Happy',
+  'I got you',
+  'Duke Dumont',
+  'Dance'
+),
+(
+  'Happy',
+  'Slide',
+  'Calvin Harris ft Frank Ocean',
+  'Dance'
+),
+(
+  'Happy',
+  'Thats what I like',
+  'Bruno Mars',
+  'Pop'
+),
+(
+  'Trappy',
+  'X',
+  '21 Savage ft Future',
+  'Rap'
+),
+(
+  'Trappy',
+  'DopeMan Go',
+  'Tory Lanez',
+  'Rap'
+),
+(
+  'Trappy',
+  'Finesse the plug',
+  'Gucci mane',
+  'Rap'
+),
+(
+  'Party',
+  'Wild thoughts',
+  'Dj Khaled',
+  'Hip hop'
+),
+(
+  'Party',
+  'Shots',
+  'LMFAO',
+  'Pop'
+),
+(
+  'Party',
+  'Signs',
+  'Drake',
+  'Hip hop'
+),
+(
+  'Wavi',
+  'Post Malone',
+  'White Iverson',
+  'Hip hop'
+),
+(
+  'Wavi',
+  'Fantasy',
+  'Urban Flora',
+  'Electronic'
+),
+(
+  'Wavi',
+  'Open Interlude',
+  'Bryson Tiller',
+  'R&B'
+),
+(
+  'Wavi',
+  'Break from Toronto',
+  'PartyNextDoor',
+  'R&B'
+);
+
